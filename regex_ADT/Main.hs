19f7ed83afea378ad206f10a441892fc637a2937
Corey O'Connor <corey@ooyala.com>
2013-10-30 00:24:07 -0700

    instead of improving the algorithm. Use this as an excuse to demonstrate how to sparse the test data set

--- regex_ADT/Main.hs
+++ regex_ADT/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Regex
@@ -7,7 +10,37 @@ import Test.Framework.Providers.SmallCheck
 import Test.SmallCheck
 import Test.SmallCheck.Series
 
+class DataSet set_id where
+    data_set :: set_id -> [String]
+
+data Set0
+
+instance DataSet Set0 where
+    data_set _ = [ "A"
+                 , "AB"
+                 , "B"
+                 , ""
+                 ]
+
+data Set1
+
+instance DataSet Set1 where
+    data_set _ = [ "X"
+                 , "XY"
+                 , "Y"
+                 , ""
+                 ]
+
+newtype SmallString set_id = SmallString String
+    deriving (Show)
+
+instance (DataSet set_id, Monad m) => Serial m (SmallString set_id) where
+    series = generate $ \n -> take n $ map SmallString $ data_set (undefined :: set_id)
+
+small_string_match :: SmallString Set0 -> SmallString Set1 -> SmallString Set0 -> Bool
+small_string_match (SmallString prefix) (SmallString s) (SmallString suffix)
+    = (prefix ++ s ++ suffix) `match` (regex s)
+
 main = defaultMain
-    [ testProperty "simple strings match" $
-        \(prefix :: String, s :: String, suffix :: String) -> True == (prefix ++ s ++ suffix) `match` (regex s)
+    [ testProperty "simple strings match" small_string_match
     ]
